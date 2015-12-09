17f44a1ecf2c7158d9e4f70ed22504fe590745b4
JustinPhlegar <Phlegar85@yahoo.com>
2015-12-09 01:18:56 -0500

    Adding comments

--- Toolkit/Utils/Base64Encoder.cs
+++ Toolkit/Utils/Base64Encoder.cs
@@ -5,15 +5,25 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Orasi.Toolkit.Utils
-{
+{ /// <summary>
+  /// Class to help quickly encode and decode text
+  /// </summary>
     class Base64Encoder
     {
+        /// <summary>
+        /// Quick helper method to encode text</summary>
+        /// <param name="plainText">Text that you want to be encoded</param>
+        /// <returns>Encoded string that is not plain text</returns>
         public static string encode(String plainText)
         {
             var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
             return System.Convert.ToBase64String(plainTextBytes);
         }
 
+        /// <summary>
+        /// Quick helper method to decode text</summary>
+        /// <param name="encodedString">Text that you want to be decoded</param>
+        /// <returns>Decoded string that is plain text</returns>
         public static string decode(String encodedString)
         {
             var base64EncodedBytes = System.Convert.FromBase64String(encodedString);
