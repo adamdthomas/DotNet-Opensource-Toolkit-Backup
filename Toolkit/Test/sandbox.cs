3ecb9d95bc7e43073b20ca4acbec5cc0dca81ac9
Paul Atkins <pa31650@yahoo.com>
2016-01-04 15:28:57 -0500

    made sandbox.cs functional

--- Toolkit/Test/sandbox.cs
+++ Toolkit/Test/sandbox.cs
@@ -1,40 +1,54 @@
 ﻿using System;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
+//using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
-using NUnit.Framework;
+//using NUnit.Framework;
 using Orasi.Toolkit.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Orasi.Toolkit
 {
-    [TestFixture]
+    //[TestFixture]
+    [TestClass]
     public class Sandbox
-
     {
-        FirefoxDriver _driver;
+        //FirefoxDriver _driver;
+        IWebDriver driver;
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("http://bluesourcestaging.heroku.com");
+        }
         // [Test]
+        [TestMethod]
         public void SampleLoginTest()
         {
-            _driver = new FirefoxDriver();
-            _driver.Navigate().GoToUrl("http://bluesourcestaging.heroku.com");
-            _driver.FindElement(By.Id("employee_username")).SendKeys("company.admin");
-            _driver.FindElement(By.Id("employee_password")).SendKeys("blah");
-            _driver.FindElement(By.XPath("//input[@value='Login']")).Click();
+            //_driver = new FirefoxDriver();
+            //_driver.Navigate().GoToUrl("http://bluesourcestaging.heroku.com");
+            driver.FindElement(By.Id("employee_username")).SendKeys("company.admin");
+            driver.FindElement(By.Id("employee_password")).SendKeys("blah");
+            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
 
-            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
             Assert.IsTrue(wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//a[text()='Logout']"))).Displayed);
         }
 
         // [TearDown]
+        [TestCleanup]
         public void TearDown()
         {
-            _driver.Quit();
+            driver.Quit();
         }
 
-        [Test]
+        //[Test]
         public void sleeper()
         {
             Sleeper.sleep(3000);
         }
+
+        
     }
 }
