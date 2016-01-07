7d7de07d6e0a03af93f10acaced30630eeb2be45
Paul Atkins <pa31650@yahoo.com>
2016-01-07 10:36:09 -0500

    updated exceldocumentreader code

--- Toolkit/Test/sandbox.cs
+++ Toolkit/Test/sandbox.cs
@@ -4,28 +4,31 @@ using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 using NUnit.Framework;
 using Orasi.Toolkit.Utils;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Orasi.Toolkit
 {
-    //[TestFixture]
+    [TestFixture]
     public class Sandbox
-
-    {
+        {
         FirefoxDriver _driver;
         [Test]
         public void SampleLoginTest()
         {
+            exceldocumentreader();
+            
             _driver = new FirefoxDriver();
             _driver.Navigate().GoToUrl("http://bluesourcestaging.heroku.com");
             _driver.FindElement(By.Id("employee_username")).SendKeys("company.admin");
             _driver.FindElement(By.Id("employee_password")).SendKeys("blah");
-            _driver.FindElement(By.XPath("//input[@value='Login']")).Click();
+            _driver.FindElement(By.XPath("//input[@value='Login']")).Submit();
 
             var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
             Assert.IsTrue(wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//a[text()='Logout']"))).Displayed);
         }
 
         [TearDown]
+        //[OneTimeTearDown]
         public void TearDown()
         {
             _driver.Quit();
@@ -35,6 +38,13 @@ namespace Orasi.Toolkit
         public void sleeper()
         {
             Sleeper.sleep(3000);
+        }
+
+        public void exceldocumentreader()
+        {
+            var edr = new ExcelDocumentReader();
+
+            edr.ReadData("C:\\Users\\Paul\\Documents\\test.xls", "panthers");
         }
     }
 }
