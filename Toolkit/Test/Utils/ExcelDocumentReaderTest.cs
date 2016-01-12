7d79a86c0ebbde22a2e7c83088691101232b5a73
Paul Atkins <pa31650@yahoo.com>
2016-01-12 09:54:15 -0500

    pending edit

--- Toolkit/Test/Utils/ExcelDocumentReaderTest.cs
+++ Toolkit/Test/Utils/ExcelDocumentReaderTest.cs
@@ -1,16 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using OpenQA.Selenium;
-using OpenQA.Selenium.Firefox;
+﻿using System.Collections.Generic;
 using NUnit.Framework;
-using NUnit.Common;
 using Orasi.Toolkit.Utils;
-using System.Resources;
-using System.IO;
 
 namespace Orasi.Toolkit.Test.Utils
 {
@@ -20,17 +10,6 @@ namespace Orasi.Toolkit.Test.Utils
         [Test]
         public void readDataFilePathAllRows()
         {
-            /* Java Base for tests
-            Object[][] data = null;
-            String filepath = getClass().getResource("/excelsheets/TestData.xlsx").getPath();
-            data = new ExcelDocumentReader(filepath).readData("Data");
-
-            Assert.assertNotNull(data);
-            Assert.assertTrue(data.length == 3);
-            Assert.assertTrue(data[0].length == 5);
-            Assert.assertTrue(data[0][0].toString().equals("DataSet1"));
-            */
-
             string filePath = @"C:\Users\Paul\Source\Repos\DotNet-Opensource-Toolkit\Toolkit\test.xls";
             string sheetName = "panther";
 
